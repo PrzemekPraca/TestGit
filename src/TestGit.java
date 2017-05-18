8f47127db2e43b3420a859b60f0d5c33e2a15eea
Przemyslaw Sobol <przemyslaw.sobol@ericsson.com>
2017-05-18 13:19:27 +0200

    Pierwszy commit

---
+++ src/TestGit.java
@@ -0,0 +1,11 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		int a=1 ,b=1,suma=0;
+		for (int i = 0; i < 10; i++) {
+			suma+=(a+i)+(b+i);
+		}
+		System.out.println(suma);
+	}
+}
